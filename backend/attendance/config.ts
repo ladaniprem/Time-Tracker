144df4b7d602c2675403783cdf2fb72ff140bbcb
Ladani prem <premladani33453@gmail.com>
2025-10-01 22:25:59 +0530

    Clear sensitive data in config.ts
    Removed sensitive information from attendance service configuration.

--- backend/attendance/config.ts
+++ backend/attendance/config.ts
@@ -2,15 +2,15 @@
 
 export const config = {
 	ATTENDANCE_FOLDER: "past_files",
-	CRM_API_URL: "https://ecs.targetcrm.co.in/modules/Mobile/api.php",
-	CRM_USERNAME: "ecsapi",
-	CRM_PASSWORD: "123456",
-	INTERAKT_API_KEY: "OHZVa1VIQkt6cFliTmRpc3NDWXM2dWd6Q0U0LTZUQTZBbFREWHlab2I2UTo=",
-	SENDER_WHATSAPP_NUMBER: "8160017115",
-	SMTP_SERVER: "smtp.gmail.com",
-	SMTP_PORT: 587,
-	SENDER_EMAIL: "dcp6996@gmail.com",
-	SENDER_PASSWORD: "oxghfwezxzqoiaqp",
+	CRM_API_URL: "",
+	CRM_USERNAME: "",
+	CRM_PASSWORD: "",
+	INTERAKT_API_KEY: "",
+	SENDER_WHATSAPP_NUMBER: "",
+	SMTP_SERVER: "",
+	SMTP_PORT: ,
+	SENDER_EMAIL: "",
+	SENDER_PASSWORD: "",
 };
 
 
